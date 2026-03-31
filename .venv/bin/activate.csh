437ed709b72192e019b41fdefaa17d23eb4c5d29
Youssef Ben Letaifa <ben.letaifa.youssef@outlook.com>
2026-03-31 09:56:43 +0100

    chore: update virtual environment configuration and paths for the Kronos project

--- .venv/bin/activate.csh
+++ .venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /home/youssef-ben-letaifa/Desktop/Kro/.venv
+setenv VIRTUAL_ENV '/home/youssef-ben-letaifa/Desktop/Kronos Project/Kro/.venv'
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
--- .venv/lib/python3.11/site-packages/numpy/version.py
+++ .venv/lib/python3.11/site-packages/numpy/version.py
@@ -2,10 +2,10 @@
 """
 Module to expose more detailed version info for the installed `numpy`
 """
-version = "2.4.3"
+version = "2.4.4"
 __version__ = version
 full_version = version
 
-git_revision = "8bcb2e72e67c343e55165e6064fe6a9dc011e954"
+git_revision = "be93fe2960dbf49b4647f5783c66d967fb2c65b5"
 release = 'dev' not in version and '+' not in version
 short_version = version.split("+")[0]
